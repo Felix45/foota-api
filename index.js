b3dcb8e9749980ac4c978503126c36970061e574
felix45 <fatonoh@gmail.com>
2025-03-25 12:50:40 +0300

    A light pages server

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const app = require('http');
+const fs   = require('fs');
+const url  = require('url');
+const http = require('http');
 
-app.createServer(function(req, resp) {
-  resp.write("<h1>Hello, World</h1>");
-  resp.end();
+const DIR = './pages';
+
+http.createServer(function(req, res) {
+  let fileName = url.parse(req.url).path;
+
+  if(fileName === '/') fileName += "index";
+  
+  fs.readFile(`${DIR}${fileName}.html`, function(err, data){
+    if(err) {
+        res.writeHead(404, {"Content-Type":"text/html"});
+        return res.end("404 Not Found");
+    }
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
