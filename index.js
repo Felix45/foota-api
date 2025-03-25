6fa465a04bf3f9e098cff6b28c356b17b6334bd7
felix45 <fatonoh@gmail.com>
2025-03-25 14:04:35 +0300

    Fix linter errors in .js file

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const fs   = require('fs');
-const url  = require('url');
+const fs = require('fs');
+const url = require('url');
 const http = require('http');
 
 const DIR = './pages';
 
-http.createServer(function(req, res) {
+http.createServer((req, res) => {
   let fileName = url.parse(req.url).path;
 
-  if(fileName === '/') fileName += "index";
-  
-  fs.readFile(`${DIR}${fileName}.html`, function(err, data){
-    if(err) {
-        res.writeHead(404, {"Content-Type":"text/html"});
-        return res.end("404 Not Found");
+  if (fileName === '/') fileName += 'index';
+
+  fs.readFile(`${DIR}${fileName}.json`, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/json' });
+      return res.end(JSON.stringify({ error: 'File not found' }));
     }
-    res.writeHead(200, {"Content-Type":"text/html"});
+    res.writeHead(200, { 'Content-Type': 'text/json' });
     res.write(data);
     return res.end();
   });
